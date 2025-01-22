d69d176c89ba284aa4c16396d9f4a21148f29051
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-22 21:43:07 +0530

    HUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
   const [sam, setSam] = useState();
   useEffect(() => {
     setSam(980);
-  }, []);
+  });
   return (
     <div>
       <h1>Your number is {sam}</h1>
