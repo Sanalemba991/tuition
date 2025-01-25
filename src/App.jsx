6fba19061abc15928be7a63e0f7879e00fe48cf9
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-25 19:31:20 +0530

    okay

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [character, setCharacter] = useState([]); 
-
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character/1")
       .then((response) => response.json())
-      .then((data) => {
-        setCharacter(data); 
-        console.log(data)
-     
-      })
-     
-  },);
-
-
+      .then((ram) => {
+        setCharacter(ram);
+        console.log(ram);
+      });
+  });
 
   return (
-    <div >
+    <div>
       <h1>{character.name}</h1>
-      <img
-        src={character.image}
-        alt={character.name}  
-      />
+      <img src={character.image} alt={character.name} />
       <p>Status: {character.status}</p>
       <p>Species: {character.species}</p>
     </div>
