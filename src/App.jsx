020d52d934e5138b22697356360c4333b07bca73
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-31 21:03:12 +0530

    OKAY

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react"; //React Hooks
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    getData().then((result) => {
-      console.log(result);
-      setData(result);
-    });
+    getData()
+      .then((result) => {
+        setData(result.results); // Set all character data
+      })
+      .catch((error) => console.error(error));
   }, []);
+
   const getData = () => {
     return new Promise((resolve, reject) => {
       fetch("https://rickandmortyapi.com/api/character")
@@ -17,11 +19,11 @@ function App() {
         .catch(() => reject("Error While Fetching"));
     });
   };
-  
+
   return (
     <div>
       <h1>Rick and Morty Characters</h1>
-      {data?.map((character) => (
+      {data.map((character) => (
         <div key={character.id}>
           <h2>Name: {character.name}</h2>
           <p>Species: {character.species}</p>
@@ -33,4 +35,3 @@ function App() {
 }
 
 export default App;
-
