7a4f522b1f94f20a94a1fcc8f836227db33a2e2c
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-31 20:58:41 +0530

    OKAY

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,23 @@ function App() {
   }, []);
   const getData = () => {
     return new Promise((resolve, reject) => {
-      fetch("https://rickandmortyapi.com/api/character/2")
+      fetch("https://rickandmortyapi.com/api/character")
         .then((response) => response.json())
         .then((data) => resolve(data))
         .catch(() => reject("Error While Fetching"));
     });
   };
+  
   return (
     <div>
-      <h1>Name: {data.name}</h1>
-      <p>Species: {data.species}</p>
+      <h1>Rick and Morty Characters</h1>
+      {data?.map((character) => (
+        <div key={character.id}>
+          <h2>Name: {character.name}</h2>
+          <p>Species: {character.species}</p>
+          <img src={character.image} alt={character.name} width="150" />
+        </div>
+      ))}
     </div>
   );
 }
